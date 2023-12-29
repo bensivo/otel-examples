5f807fe1443271889a4a3d1cf70e2229c087a774
Ben Sivoravong <bensivo@gmail.com>
2023-12-28 20:32:08 -0600

    Updated comments

--- nodejs/tracing/app/sender.ts
+++ nodejs/tracing/app/sender.ts
@@ -5,15 +5,19 @@ const tracer = new OtelTracer('nodejs-tracing-demo-sender', '0.0.0');
 tracer.initialize();
 
 async function main() {
-    // Simulate some multi-step pipeline. 
-    //   3 sequential jobs: job-a, job-b, job-c
-    //      job-c includes 3 instances of a task, task-c, run in parallel
     const span = tracer.span('pipeline', {});
     const context = tracer.context(span);
 
     console.log('Starting jobs')
 
     for (let i = 0; i < 3; i++) {
+        // The child jobs for this demo are in another application, so instead of passing
+        // the context object directly, we have to serialize it.
+        //
+        // We serialize the context with tracer.toB3(), and pass that along with our HTTP request.
+        // 
+        // NOTE: traditionally you use HTTP headers for b3, but we're using the body here to show that
+        // this system is not HTTP specific, you could use any transport mechanism you wanted.
         await axios({
             method: 'POST',
             url: 'http://localhost:3000/job',
